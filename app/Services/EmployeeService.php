970e9a23f3b8ea56c0b4811a50eb1b170d64fd14
hadas728 <hadas72802@gmail.com>
2024-06-19 13:39:20 +0300

    validation rules

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -5,8 +5,7 @@ namespace App\Services;
 use App\DTO\EmployeeDTO;
 use App\Models\Details;
 use App\Models\Employee;
-use App\Validators\DetailsValidator;
-use Carbon\Carbon;
+use App\Validators\EmployeeValidator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
@@ -25,28 +24,19 @@ class EmployeeService
         }
 
         // Fetch employees with requested columns or all columns if none specified
-        $employees = $requestedColumns ?
-            Employee::select($requestedColumns)->get() :
-            Employee::all();
+        $employees = $requestedColumns
+            ? Employee::leftJoin('details', 'employees.id', '=', 'details.employee_id')
+            ->select($requestedColumns)
+            ->get()
+            : Employee::leftJoin('details', 'employees.id', '=', 'details.employee_id')
+            ->get();
 
         return response()->json($employees);
     }
 
-    // public function updateData()
-    // {
-    //     $employees = Employee::query()
-    //         ->get();
-
-    //     foreach ($employees as $employee) {
-    //         $employee->update([
-    //             'updated_at' => Carbon::now()
-    //         ]);
-    //     }
-    // }
-
     public function update()
     {
-        $filePath = "C:\\Users\\Emet-Dev\\Documents\\New folder\\employees.csv";
+        $filePath = "C:\\Users\\Emet-Dev-23\\Desktop\\Projects\\employees.csv";
         // Column mapping from CSV to database
         $columnMapping = [
             'תז' => 'personal_id',
@@ -71,39 +61,32 @@ class EmployeeService
 
         if (file_exists($filePath)) {
             $fileContents = file($filePath);
-            
             $headers = str_getcsv(array_shift($fileContents));
             $dbHeaders = array_map(function ($header) use ($columnMapping) {
                 return $columnMapping[$header] ?? null;
             }, $headers);
-            
+
             $employeeDTOs = [];
             $csvPersonalIds = [];
             foreach ($fileContents as $line) {
                 $data = str_getcsv($line);
                 $employeeData = array_combine($dbHeaders, $data);
-                
-                if (!DetailsValidator::validate($employeeData)) {
+
+                if (!EmployeeValidator::validate($employeeData)) {
                     continue;
                 }
-                
+
                 $employeeDTO = new EmployeeDTO($employeeData);
-                
+
                 $employeeDTOs[] = $employeeDTO;
                 $csvPersonalIds[] = $employeeDTO->personal_id;
-                // $csvPersonalNumbers[] = $employeeData["personal_number"];
             }
-            
+
             DB::beginTransaction();
             try {
-                // Soft delete employees not in the CSV
-                // Employee::whereNotIn('personal_number', $csvPersonalNumbers)->delete();
                 Details::whereNotIn('personal_id', $csvPersonalIds)->delete();
-                
-                
-                // Save all valid DTOs to the database using updateOrCreate
+
                 foreach ($employeeDTOs as $dto) {
-                    // dd($dto);
                     $employee = Employee::withTrashed()->updateOrCreate(
                         ['personal_number' => $dto->personal_number],
                         (array)$dto
