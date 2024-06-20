dd86cd96ea9e22f91e7bba0519d363b6cba90061
hadas728 <hadas72802@gmail.com>
2024-06-20 09:55:46 +0300

    users and passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
+        // Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
     }
 }
--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -58,57 +58,57 @@ class EmployeeService
             'מזהה אוכלוסיה' => 'population_id',
             'טלפון' => 'phone_number',
         ];
-
+        
         if (file_exists($filePath)) {
             $fileContents = file($filePath);
             $headers = str_getcsv(array_shift($fileContents));
             $dbHeaders = array_map(function ($header) use ($columnMapping) {
                 return $columnMapping[$header] ?? null;
             }, $headers);
-
+            
             $employeeDTOs = [];
             $csvPersonalIds = [];
             foreach ($fileContents as $line) {
                 $data = str_getcsv($line);
                 $employeeData = array_combine($dbHeaders, $data);
-
+                
                 if (!EmployeeValidator::validate($employeeData)) {
                     continue;
                 }
-
+                
                 $employeeDTO = new EmployeeDTO($employeeData);
-
+                
                 $employeeDTOs[] = $employeeDTO;
                 $csvPersonalIds[] = $employeeDTO->personal_id;
             }
-
+            
             DB::beginTransaction();
             try {
                 Details::whereNotIn('personal_id', $csvPersonalIds)->delete();
-
+                
                 foreach ($employeeDTOs as $dto) {
                     $employee = Employee::withTrashed()->updateOrCreate(
                         ['personal_number' => $dto->personal_number],
                         (array)$dto
                     );
-
+                    
                     $dto->employee_id = $employee->id;
-
+                    
                     $details = Details::withTrashed()->updateOrCreate(
                         ['personal_id' => $dto->personal_id],
                         (array)$dto
                     );
-
+                    
                     // Restore if it was soft deleted
                     if ($employee->trashed()) {
                         $employee->restore();
                         $details->restore();
                     }
                 }
-
+                
                 // Commit the transaction
                 DB::commit();
-
+                
                 return redirect()->back()->with('success', 'CSV file imported successfully.');
             } catch (\Exception $e) {
                 // Rollback the transaction on error
